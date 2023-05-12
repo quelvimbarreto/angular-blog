07e6ab4003d8db8028ea2fd5ac4608f9b8f19c54
Quelvim Uiliam Barreto dos Santos <quelvimbarreto@gmail.com>
2023-05-12 16:36:36 -0300

    Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { TitleComponent } from './components/title/title.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-	declarations: [AppComponent],
+	declarations: [AppComponent, MenuComponent, TitleComponent, BigCardComponent, SmallCardComponent, HomeComponent],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
 	bootstrap: [AppComponent],
